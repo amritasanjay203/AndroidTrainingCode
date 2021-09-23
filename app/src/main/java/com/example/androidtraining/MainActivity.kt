d07538ddb057c85a94a4fdb81febfd6231266b89
CODE1\320106678 <amrita.sanjay@philips.com>
2021-09-23 14:20:32 +0530

    EditText and TextView added. Text in EditText displayed in TextView on button click

--- app/src/main/java/com/example/androidtraining/MainActivity.kt
+++ app/src/main/java/com/example/androidtraining/MainActivity.kt
@@ -4,22 +4,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mButton: Button
+    lateinit var etName: EditText
+    lateinit var tvRes: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // inflating layout
 
-        mButton = findViewById(R.id.button)
+        etName = findViewById(R.id.etName)
+        tvRes = findViewById(R.id.textView)
+//        mButton = findViewById(R.id.button)
         // Step 1 -> User has to click the button
         // Step 2 -> After Step 1 (user clicks), show the toast
 
         // OnClickListener is a callback
-        mButton.setOnClickListener(View.OnClickListener {
-            Toast.makeText(this, "button was clicked", Toast.LENGTH_SHORT).show()
-        })
+//        mButton.setOnClickListener(View.OnClickListener {
+//            Toast.makeText(this, "button was clicked", Toast.LENGTH_SHORT).show()
+//        })
     }
+
+    fun clickHandler(view: View) {
+        //get the data from edittext
+        val name:String = etName.text.toString()
+        //put that data into the textview
+        tvRes.text = name
+    }
+
 }
