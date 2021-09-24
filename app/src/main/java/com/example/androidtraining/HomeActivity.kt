218e115e1ce1b35c0d9a2233379acc9ff9aca8ac
CODE1\320106678 <amrita.sanjay@philips.com>
2021-09-24 11:46:42 +0530

    Start activity for result

--- app/src/main/java/com/example/androidtraining/HomeActivity.kt
+++ app/src/main/java/com/example/androidtraining/HomeActivity.kt
@@ -1,19 +1,36 @@
 package com.example.androidtraining
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
     lateinit var tvHome: TextView
+    lateinit var etContact: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        tvHome = findViewById(R.id.tvHome)
+//        tvHome = findViewById(R.id.tvHome)
+        etContact = findViewById(R.id.etContact)
 
-        val name = intent.getStringExtra("namekey")
-        tvHome.setText(name)
+//        val name = intent.getStringExtra("namekey")
+//        tvHome.setText(name)
+    }
+
+    fun handleClick(view: View) {
+        //get the contact from edittext
+        var data = etContact.text.toString()
+        //put the contact in a intent
+        var intent:Intent = Intent()
+        intent.putExtra("contactkey",data)
+        //send the data to the parent activity[mainactivity]
+        setResult(RESULT_OK,intent) //result code -- smell/ it'll inform the parent activity whether the result is good for consumption
+        //finish/close this activity
+        finish()
     }
 }
--- app/src/main/java/com/example/androidtraining/MainActivity.kt
+++ app/src/main/java/com/example/androidtraining/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.androidtraining
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -31,10 +33,31 @@ class MainActivity : AppCompatActivity() {
 
     fun clickHandler(view: View) {
         // setTextView()
-        val data = etName.text.toString()
-        val hIntent: Intent = Intent(this,HomeActivity::class.java) // Explicit intent
-        hIntent.putExtra("namekey",data)
-        startActivity(hIntent)
+        when (view.id) {
+            R.id.button -> {
+                startHome()
+            }
+            R.id.buttonDial -> {
+                val dialIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:1234567")) //implicit intent
+                startActivity(dialIntent)
+            }
+//            R.id.buttonAlarm -> {
+//                createAlarm("Philips",17,15)
+//            }
+//            R.id.buttonMaps -> {
+//                showMap(Uri.parse("geo:47.6,-122.3"))
+//            }
+        }
+
+    }
+
+    private fun startHome() {
+//        val data = etName.text.toString()
+        val hIntent: Intent = Intent(this, HomeActivity::class.java) // Explicit intent
+//        hIntent.putExtra("namekey", data)
+//        startActivity(hIntent)
+        startActivityForResult(hIntent,123) //what is request code--123
+        //for eg on whatsapp -- camera,contact, location the return point is onActivityREsult
     }
 
     private fun setTextView() {
@@ -44,9 +67,37 @@ class MainActivity : AppCompatActivity() {
         tvRes.text = name
     }
 
-    fun clickClock(view: View) {
-        val clockIntent = Intent(Intent.ACTION_QUICK_CLOCK) // Implicit intent
-        startActivity(clockIntent)
+//    fun clickClock(view: View) {
+//        val clockIntent = Intent(Intent.ACTION_QUICK_CLOCK) // Implicit intent
+//        startActivity(clockIntent)
+//    }
+
+//    fun createAlarm(message: String, hour: Int, minutes: Int) {
+//        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+//            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+//            putExtra(AlarmClock.EXTRA_HOUR, hour)
+//            putExtra(AlarmClock.EXTRA_MINUTES, minutes)
+//        }
+//        if (intent.resolveActivity(packageManager) != null) {
+//            startActivity(intent)
+//        }
+//    }
+//
+//    fun showMap(geoLocation: Uri) {
+//        val intent = Intent(Intent.ACTION_VIEW).apply {
+//            data = geoLocation
+//        }
+//        if (intent.resolveActivity(packageManager) != null) {
+//            startActivity(intent)
+//        }
+//    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+        //request code helps you determine from which childactivity[camera/contact] is returning the data
+        //request code = 123, result code =RESULT_OK
+        var data = intent?.getStringExtra("contactkey")
+        tvRes.text = data
     }
 
 }
